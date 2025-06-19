92a9dd471d11d6dc9d7cfa571f9120dd4dc605c3
Romulo Rosa | FullStack Developer <romulorosafullstack@gmail.com>
2025-06-18 21:26:57 -0300

    create GlobalStyles Settings Colors

---
+++ src/GlobalStyles/Settings/Colors/index.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const Colors = createGlobalStyle`
+  html {
+    --color-black: #000;
+    --color-blue:#014bd5;
+    --color-green:#178401;
+    --color-red: #d5012b;
+    --color-white: #FFF;
+  }
+`
+
+export default Colors
