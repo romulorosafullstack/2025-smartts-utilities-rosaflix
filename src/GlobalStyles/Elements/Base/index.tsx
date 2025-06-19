218099173683f6572583621dc1ca9b05605992ad
Romulo Rosa | FullStack Developer <romulorosafullstack@gmail.com>
2025-06-18 21:26:39 -0300

    create GlobalStyles Elements Base

---
+++ src/GlobalStyles/Elements/Base/index.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const Base = createGlobalStyle`
+  body{
+    background-color: var(--color-blue);
+    color: var(--color-white);
+  }
+`
+
+export default Base
