b9c96261659b82dda67e5e96a4035443de21f34d
Romulo Rosa | FullStack Developer <romulorosafullstack@gmail.com>
2025-06-24 11:44:36 -0300

    zerando os components para novo layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,8 @@
-import styled from "styled-components";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import CadastrarFilme from "./pages/CadastrarFilme/CadastrarFilme";
-
-
-const App__Wrapper = styled.div`
-  background-color: #000;
-  display: flex;
-  justify-content: center;
-  height: 100vh;
-  width: 100vw;
-`
-
-const App__Container = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: space-between;
-  width: 100vw;
-`
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
-    <App__Wrapper>
-      <App__Container>
-        <Header />
-        <CadastrarFilme />
-        <Footer />
-      </App__Container>
-    </App__Wrapper>
+    <Header />
   )
 }
 
