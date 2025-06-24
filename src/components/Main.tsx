5f4c6813f8a6112e95313050f9c187cbb0d13b23
Romulo Rosa | FullStack Developer <romulorosafullstack@gmail.com>
2025-06-23 23:19:10 -0300

    component main chamando movie

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,11 @@
+import Movie from "./Movie";
+
 export function Main(){
   return(
     <main>
-      <h1>Main</h1>
+      <Movie title="Star Wars" year={1980} director="George Lucas" genre="Ficção" />
+      <Movie title="Star Wars 2" year={1980} director="Spilberg" genre="Ficção" />
+      <Movie title="Star Wars 3" year={1980} director="Stallone" genre="Ficção"/>
     </main>
   )
 }
