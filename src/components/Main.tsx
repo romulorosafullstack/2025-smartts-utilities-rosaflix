ac4ef6db271bdb58184d8dbbbb1291ae6c2d8f40
Romulo Rosa | FullStack Developer <romulorosafullstack@gmail.com>
2025-06-24 07:56:51 -0300

    component Main chamando FormRegisterMovie

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,21 @@
+import { FormRegisterMovie } from "./FormRegisterMovie";
 import Movie from "./Movie";
+
+
+import styled from "styled-components";
+
+const Main__Wrapper = styled.header`
+  padding: 3rem 15rem;
+  width: 100%;
+`
 
 export function Main(){
   return(
-    <main>
+    <Main__Wrapper>
       <Movie title="Star Wars" year={1980} director="George Lucas" genre="Ficção" />
       <Movie title="Star Wars 2" year={1980} director="Spilberg" genre="Ficção" />
       <Movie title="Star Wars 3" year={1980} director="Stallone" genre="Ficção"/>
-    </main>
+      <FormRegisterMovie />
+    </Main__Wrapper>
   )
 }
