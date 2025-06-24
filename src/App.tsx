dfa9339ded08417f6ad684265fd3cea2c90ceacf
Romulo Rosa | FullStack Developer <romulorosafullstack@gmail.com>
2025-06-23 22:08:56 -0300

    app chamando header main logo com styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-export default function App() {
+import styled from "styled-components";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
 
+const App__Wrapper = styled.div`
+  background-color: #000;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`
+
+const App__Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 100vw;
+`
+
+
+export default function App() {
   return (
-    <>
-      <h1>Novo App</h1>
-    </>
+    <App__Wrapper>
+      <App__Container>
+        <Header />
+        <Main />
+        <Footer />
+      </App__Container>
+    </App__Wrapper>
   )
 }
