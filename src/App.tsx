011e0f996acfb0edf3999b945c266af274f4ad49
Romulo Rosa | FullStack Developer <romulorosafullstack@gmail.com>
2025-06-26 15:47:48 -0300

    criacao das rotas Home e Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import Header from "./components/Header/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <Header />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
